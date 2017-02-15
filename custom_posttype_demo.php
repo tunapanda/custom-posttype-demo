affd5d321ae8f042c3eb445c7fddbb931e7a60a7
Mikael Lindqvist <li.mikael@gmail.com>
2017-02-15 13:17:52 +0300

    display posts

--- custom_posttype_demo.php
+++ custom_posttype_demo.php
@@ -7,6 +7,12 @@ Description: A small demo of how to create a plugin that extends WordPress with 
 Version: 0.0.1
 */
 
+/****************************************************************************
+ *
+ *  PART 1 - Registering the posttype
+ *
+ ****************************************************************************/
+
 /**
  * This is the function we will later register to listen to the WordPress
  * init action. In the function, we call the register_post_type function
@@ -38,3 +44,41 @@ function custom_posttype_init() {
  * Register the function above so that it is called on the init event.
  */
 add_action("init","custom_posttype_init");
+
+/****************************************************************************
+ *
+ *  PART 2 - Showing instances of the posttype on the WordPress site.
+ *
+ ****************************************************************************/
+
+/**
+ * We will register this function to listen to the the_content filter. This 
+ * function will be called when a user actually looks at a thing by visiting
+ * its URL, also known as permalink, in the browser. It is actually called
+ * when a user wants to look at any kind of post in WordPress, so we need
+ * to make sure we don't mess up how other posts will appear. Once we make
+ * sure that the request is actually for a thing, we can return any HTML
+ * we want to represent how the thing should appear.
+ */
+function custom_posttype_content($content) {
+	$post=get_post();
+
+	// Make sure that we are actually requested to show a post that is an
+	// instance of our custom posttype. If it isn't, just return the content
+	// that was passed as a parameter, so we don't mess up other posttypes.
+	if ($post->post_type!="thing")
+		return $content;
+
+	$output=
+		"This is how a user sees a thing. We don't have so much information ".
+		"about things, but we can tell you the title: ".
+		"<b>".$post->post_title."</b><br>".
+		"</b>And the id: <b>".$post->ID."</b>";
+
+	return $output;
+}
+
+/*
+ * Register the function above so that it is called on the the_content event.
+ */
+add_filter("the_content","custom_posttype_content");
